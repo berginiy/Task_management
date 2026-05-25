62c93d95609fad8327629e62feae16b6bdc43f75
berginiy <pirozokaltynbek@example.com>
2026-05-25 21:27:12 +0600

    CORS

--- task-management/src/main/java/com/company/task_management/config/SecurityConfig.java
+++ task-management/src/main/java/com/company/task_management/config/SecurityConfig.java
@@ -75,7 +75,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // твой фронт (Vite)
+
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://task-management-frontend-07x9.onrender.com"
+        ));
+
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
